7b3f537d5432b919e6924ff956a9ef1c5c9e90f5
Daniel Kessler <sinani201@gmail.com>
2015-10-07 09:19:26 -0700

    added librejs licensing information in site.js

--- site.js
+++ site.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+
 function removeChildren(parent) {
 	while (parent.firstChild) {
 		parent.removeChild(parent.firstChild);
@@ -133,3 +135,4 @@ window.onload = function () {
 
 	document.getElementById("replay").onclick = replaySongSnippet;
 };
+// @license-end
